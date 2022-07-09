b5a87e6bfb60f522a3117b0c0827916c1fc934bb
misiucodes <meeshtran15@gmail.com>
2022-07-09 11:14:42 -0400

    integrated leafly map

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent; // assign empty variable to be reassigned in event listeners
+
+
 // GEOLOCATION /////////
 
 // Accepts 2 call back functions, success or else
@@ -22,38 +25,52 @@ navigator.geolocation.getCurrentPosition(
     const coords = [latitude, longitude];
 
     // Parameter is the HTML element id, value in setView is zoom level
-    const map = L.map('map').setView(coords, 13);
+    map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker(coords)
-    .addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
-
-    // Event Listener from Leaflet Library
-    map.on('click', function(mapEvent) {
-      console.log(mapEvent);
-      const {lat, lng} = mapEvent.latlng;
-
-      L.marker([lat, lng])
-      .addTo(map)
-      // Pass in object to customize popup style Leaflet css library
-      .bindPopup(L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup',
-      })
-      )
-      .setPopupContent('Workout')
-      .openPopup();
+    // Leaflet Event Listener - handling clicks on map
+    map.on('click', function(mapE) {
+      // Show form on map click
+      mapEvent = mapE;
+      form.classList.remove('hidden');
+      inputDistance.focus(); // enables blinking cursor in distance field
     });
-}, 
+  }, 
   function() {
-    alert('Could not get your location');
-  }
-);
+      alert('Could not get your location');
+    }
+  );
+  
+  // Event Listener - handling popup marker upon form submit
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // Clear input fields
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+
+    // Display marker
+    const {lat, lng} = mapEvent.latlng;
+  
+    L.marker([lat, lng])
+    .addTo(map)
+    // Pass in object to customize popup style from Leaflet library
+    .bindPopup(L.popup({
+      maxWidth: 250,
+      minWidth: 100,
+      autoClose: false,
+      closeOnClick: false,
+      className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
+
+// Event Listener - handling switch from running to cycling
+inputType.addEventListener('change', function() {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
